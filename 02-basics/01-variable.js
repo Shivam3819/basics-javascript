d6f3764f2d960a3988ba62ee1d4e9edb45a8b7b1
shivam kumar <Shivam20031215@gmail.com>
2025-08-05 17:31:02 +0000

    variable implementation

---
+++ 02-basics/01-variable.js
@@ -0,0 +1,11 @@
+let accountId=27287677
+let accountEmail="shivam@12"
+let accountPassword="38378"
+accountCity="chhapra"
+
+accountId=3993939
+accountEmail="shiva@8"
+accountPassword="58378"
+accountCity="chhapra"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
