b5e7449634039c9beb689a69e252cb9d422178a0
shivam kumar <Shivam20031215@gmail.com>
2025-08-18 17:20:43 +0000

    datatype conversion

---
+++ 02-basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+//let score=true;
+
+//console.log(typeof score);
+//let valueinNumber= Number(score)
+//console.log(valueinNumber)
+//console.log(typeof valueinNumber)
+
+let isLoggedIn= 1;
+console.log(isLoggedIn)
+let convert= Boolean(isLoggedIn)
+console.log( convert)
+let number=38;
+
+let numberToString=String(number)
+console.log( typeof numberToString)
